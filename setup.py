0a0b9760ae62cd5a776ea1baa460ecaf17e9109d
Alejandro Queiruga <afqueiruga@lbl.gov>
2018-10-26 16:14:33 -0700

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "HydrogeologyTesting",
+    version = "0.0.4",
+    author = "Alejandro Francisco Queiruga",
+    description = "",
+    license = "none",
+    keywords = "",
+    packages=['HydrogeologyTesting'],
+    long_description=read('Readme.md'),
+    classifiers=[],
+)
