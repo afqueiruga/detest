ab4e728d66a1135408c0242b76ad1a6d355cd1fb
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-11-05 19:19:17 -0800

    Laying out some more oracles

---
+++ HydrogeologyTesting/oracles/heat_equation.py
@@ -0,0 +1,69 @@
+description = """
+This file contains the analytical solution to the heat equation on a square
+domain.
+
+The solutions it returns are of T(x,y,t).
+
+dT/dt = div k grad T + f
+
+This is a good one to converge to numerically.
+"""
+
+default_parameters = {
+    'K_d': 10.0e9,
+    'K_s': 100.0*10.0e9,
+    'K_f':1.0e11,
+    'phi':0.22,
+    'k':1.0e-13,
+    'eta':1.0e-3,
+
+    'source':-1.0e5,
+}
+
+class SquareHomogenousHeatEquation():
+    name = 'StaticSquarePoisson'
+    space_dim = 2
+    time_dep = False
+    ptdim = 3
+    def __init__(self,params=default_parameters):
+        self.params = params
+        # Yoink out the parameters
+        K_d = params['K_d']
+        K_s = params['K_s']
+        K_f = params['K_f']
+        phi = params['phi']
+        k_eta = params['k']/params['eta']
+        domH = 1.0
+        self.source = params['source']
+        # Only onle coefficent
+        alpha = biot = 1.0 - K_d / K_s
+        M = K_s/(alpha-phi*(1.0-K_s/K_f))
+        self.k = k_eta / self.source
+    def __call__(self, xt):
+        pass
+        return {'P':self.P(xt[:,0],xt[:,1], xt[:,2])}
+
+class SquareForcedHeatEquation():
+    name = 'StaticSquarePoisson'
+    space_dim = 2
+    time_dep = False
+    ptdim = 3
+    def __init__(self,params=default_parameters):
+        self.params = params
+        # Yoink out the parameters
+        K_d = params['K_d']
+        K_s = params['K_s']
+        K_f = params['K_f']
+        phi = params['phi']
+        k_eta = params['k']/params['eta']
+        domH = 1.0
+        self.source = params['source']
+        # Only onle coefficent
+        alpha = biot = 1.0 - K_d / K_s
+        M = K_s/(alpha-phi*(1.0-K_s/K_f))
+        self.k = k_eta / self.source
+    def __call__(self, xt):
+        pass
+        return {'P':self.P(xt[:,0],xt[:,1], xt[:,2])}
+
+tests = [SquareHomogenousHeatEquation, SquareForcedHeatEquation]
--- HydrogeologyTesting/oracles/square_poisson.py
+++ HydrogeologyTesting/oracles/square_poisson.py
@@ -1,12 +1,9 @@
 import numpy as np
 
-name = "terzaghi"
-
 description = """
-This file contains the analytical solution to Terzaghi's consolidation problem.
+This file contains the analytical solution to the square Poisson problem.
 
-It's a dynamic problem with one spatial dimension, height.
-The solutions it returns are P(z,t) and U(z,t).
+The solutions it returns are P(x,y).
 
 This is a good one to converge to numerically.
 """
@@ -23,12 +20,11 @@ default_parameters = {
 }
 
 class SquarePoisson():
-    name = 'SquarePoisson'
+    name = 'StaticSquarePoisson'
     space_dim = 2
-    time_dep = True
+    time_dep = False
     ptdim = 3
     def __init__(self,params=default_parameters):
-
         self.params = params
         # Yoink out the parameters
         K_d = params['K_d']
@@ -36,30 +32,21 @@ class SquarePoisson():
         K_f = params['K_f']
         phi = params['phi']
         k_eta = params['k']/params['eta']
-
         domH = 1.0
-        source = params['source']
-
-        # Poisson Ratio
-        nu = (3.0*K_d - 2.0*G)/(6.0*K_d+2.0*G)
-
+        self.source = params['source']
+        # Only onle coefficent
         alpha = biot = 1.0 - K_d / K_s
-
         M = K_s/(alpha-phi*(1.0-K_s/K_f))
-
-
+        self.k = k_eta / self.source
     def __call__(self, xt):
-        k = self.params['k']/self.params['eta']
+        k = self.k
         panal = np.empty(X.shape[0])
         t = lambda k,x,y : np.sin(k*np.pi*(1.0+x)/2.0)/( k*k*k * np.sinh( k*np.pi )) * \
             ( np.sinh( k*np.pi*(1.0+y)/2.0 ) + np.sinh( k*np.pi*(1.0-y)/2.0) )
         for i,y in enumerate(X):
-            panal[i] = (1.0-y[0]**2.0)/2.0 - 16.0/(np.pi)**3 * \
-                sum([t(k,y[0],y[1]) for k in xrange(1,101,2) ])
-        return panal
-
-        return {'P':self.P(xt[:,0],xt[:,1]),
-                'U':self.P(xt[:,0],xt[:,1])}
+            panal[i] = (1.0-xt[0]**2.0)/2.0 - 16.0/(np.pi)**3 * \
+                sum([t(k,yt[0],yt[1]) for k in xrange(1,101,2) ])
+        return {'P':panal}
 
 
-tests = [Terzaghi]
+tests = [SquarePoisson]
