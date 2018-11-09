d20e7f7bddbbf171365fbdafe4d78a21d1b2a3f9
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-11-08 16:48:58 -0800

    Added a single phase well

--- detest/oracles/__init__.py
+++ detest/oracles/__init__.py
@@ -1,2 +1,3 @@
 from . import mechanics_constant, poroelastic_constant, \
-              terzaghi, poroelastic_well, odes
+              terzaghi, poroelastic_well, single_phase_well, \
+              odes
---
+++ detest/oracles/single_phase_well.py
@@ -0,0 +1,57 @@
+"""
+Constant flow production from an infinitely large reservoir, without poromechanics.
+
+
+"""
+
+import numpy as np
+from scipy.special import exp1
+
+default_parameters = {
+    'K_d': 10.0e9,
+    'G': 10.0e9,
+    'K_s': 100.0*10.0e9,
+    'K_f':1.0e11,
+    'phi':0.22,
+    'k':1.0e-13,
+    'eta':1.0e-3,
+
+    'Q':-1.0e5,
+    'P_background' : 9.5e6,
+}
+
+class SinglePhaseWell():
+    name = "SinglePhaseWell"
+    space_dim = 1
+    time_dep = True
+    ptdim = 2
+    def __init__(self,params=default_parameters):
+        self.params = params
+        # Yoink out the parameters
+        K_d = params['K_d']
+        G = params['G']
+        K_s = params['K_s']
+        K_f = params['K_f']
+        phi = params['phi']
+        k_eta = params['k']/params['eta']
+
+        P_background = params['P_background']
+        Q_s = params['Q']
+        alpha = 1.0 - K_d/K_s
+
+        M = K_s/(alpha-phi*(1.0-K_s/K_f))
+        K_u = alpha**2*M + K_d
+        H = K_u + 4.0*G/3.0
+        D = k_eta*(M - alpha**2 * M**2 /H)
+
+        # TODO This is wrong
+        def P(r,t):
+            xi = r**2/(4.0*D*t)
+            return Q_s / (4.0*np.pi*k_eta) * exp1(xi) + P_background
+
+        self.P = P
+
+    def __call__(self, xt):
+        return {'P':self.P(xt[:,0],xt[:,1])}
+
+tests = [ SinglePhaseWell ]
