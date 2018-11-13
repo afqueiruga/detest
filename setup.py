d4b666ef88622eaf80e3f7b59c31f8b2bbb38cdf
Alejandro Queiruga <afqueiruga@lbl.gov>
2018-11-12 16:44:27 -0800

    From the module rename

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "HydrogeologyTesting",
+    name = "detest",
     version = "0.0.1",
     author = "Alejandro Francisco Queiruga",
     description = "",
     license = "none",
     keywords = "",
-    # packages=['HydrogeologyTesting'],
     packages = find_packages(exclude=['test']),
     test_suite='test',
     long_description=read('Readme.md'),
