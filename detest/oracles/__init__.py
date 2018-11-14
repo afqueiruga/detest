7dacdd2c98c3fed9dce93965732a84c46891b71a
Alejandro Queiruga <afqueiruga@lbl.gov>
2018-11-13 17:03:37 -0800

    less verbose module structure

--- detest/oracles/__init__.py
+++ detest/oracles/__init__.py
@@ -1,3 +1,11 @@
 from . import mechanics_constant, poroelastic_constant, \
               terzaghi, poroelastic_well, single_phase_well, \
               odes
+
+
+from .mechanics_constant import Uniaxial, Shear
+from .poroelastic_constant import UndrainedUniaxial, UndrainedShear
+from .terzaghi import Terzaghi
+from .poroelastic_well import PoroelasticWell
+from .single_phase_well import SinglePhaseWell
+from .odes import Decay, Oscillator
