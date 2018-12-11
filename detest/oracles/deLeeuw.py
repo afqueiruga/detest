590b7c77e1f8495385bb964ae2dac26338b68ea9
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-12-11 15:51:20 -0800

    Fixed some terms, added more roots

--- detest/oracles/deLeeuw.py
+++ detest/oracles/deLeeuw.py
@@ -54,30 +54,36 @@ class DeLeeuw():
 
         alpha = 1.0-K_d/K_s
         S  = phi/K_f+(alpha-phi)/K_s
-        p0 = alpha / ( alpha**2 + S*(K_d+1.0/3.0*G) ) * Load
-        m  = 0.5*eta*(alpha**2+S*(K_d+G/3))/(alpha**2)
-        c  = ( k/eta )/(  S+alpha**2/(K_d+4.0/3.0*G)  )
-
+        p0 = -alpha / ( alpha**2 + S*(K_d+1.0/3.0*G) ) * Load
+        m  = 0.5* ( K_d+4.0*G/3.0 )/( 2.0*G )  * (alpha**2+S*(K_d+G/3.0))/(alpha**2)
+        # c  = ( k/eta )/(  S+alpha**2/(K_d+4.0/3.0*G)  )
+        c = (k/eta) * ( K_d+4.0*G/3.0 )/( alpha**2 + S*(K_d+4.0*G/3.0) )
         var('xi')
         J0 = lambda x : besselj(0,x)
         J1 = lambda x : besselj(1,x)
         g=(2*m*xi*J0(xi)-J1(xi))
-        xi_j = [ nsolve(g,xi,j) for j in np.linspace(0,39,14) ]
-
+        # xi_j = [ nsolve(g,xi,j) for j in np.linspace(0,39,14) ]
+        xi_j = [nsolve(g,xi,0)]
+        for j in range(50):
+            last = xi_j[-1]
+            new = nsolve(g,xi,(last+1.0e-4,last+(1.2*pi.evalf())),solver='bisect' )
+            xi_j.append(new)
+        print(xi_j)
         term = lambda r,t,j : ( J0(xi_j[j])-J0(xi_j[j]*r/R) )/\
             ( (1-m*xi_j[j]**2-0.25*m)*J0(xi_j[j]) ) \
             * exp(-xi_j[j]**2*c*t/R**2)
 
         def P(r,t):
-            return np.array([ sum([float(term(r_,t_,i).evalf()) for i in range(1,13)])
-                        for r_,t_ in zip(r,t) ])
+            return np.array([
+                sum([float(p0*term(r_,t_,i).evalf()) for i in range(len(xi_j))])
+                for r_,t_ in zip(r,t) ]) + params['P_background']
         def U(r,t):
             # TODO put in the u solution. Is there one?
             return np.array([0.0])
+        from IPython import embed ; embed()
         self.P = P
         self.U = U
     def __call__(self, xt):
-        print('del P ', self.P(xt[:,0],xt[:,1]))
         return {'P':self.P(xt[:,0],xt[:,1])}
                 # 'U':self.U(xt[:,0],xt[:,1])}
 
