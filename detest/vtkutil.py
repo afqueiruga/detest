9826c345c238e5843bafe6398d8d7f3c465c04dc
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-12-13 16:38:46 -0800

    Helper utilities for parsing vtk files

---
+++ detest/vtkutil.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import glob, re
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy
+
+def list_files(fpattern):
+    files = glob.glob(fpattern)
+    grab_digit = lambda f : int(re.search("([0-9]*)\.vtk",f).groups()[-1])
+    files.sort(key=lambda f: grab_digit(f) )
+    return files
+
+def iterate(fpattern):
+    files = list_files(fpattern)
+    for f in files:
+        yield get_data(f)
+
+def get_data(fname):
+    r=vtk.vtkUnstructuredGridReader()
+    r.SetFileName(fname)
+    r.ReadAllFieldsOn()
+    r.ReadAllScalarsOn()
+    r.ReadAllVectorsOn()
+    r.Update()
+    op = r.GetOutput()
+    x = vtk_to_numpy( op.GetPoints().GetData() )
+    pd = op.GetPointData()
+    n = pd.GetNumberOfArrays()
+    dic = { pd.GetArrayName(i):vtk_to_numpy( pd.GetArray(i) ) for i in range(n) }
+    return x,dic
+
+def cat_with_times(fpattern,timefile, fields):
+    with open(timefile,"r") as f:
+        times = [ float(l) for l in f ]
+    for dat in iterate(fpattern):
+        pass
